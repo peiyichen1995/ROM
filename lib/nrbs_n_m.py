654c51b5289e7028db6c6273990efceda548ea25
peiyi <peiyic03@gmail.com>
2023-02-20 11:47:16 -0500

    fast bubble and convolve func for batch_size = 1

--- lib/nrbs_n_m.py
+++ lib/nrbs_n_m.py
@@ -8,119 +8,86 @@ torch.set_default_dtype(torch.float64)
 
 # bandwidth: n x m
 class NRBS(torch.nn.Module):
-    def __init__(self, N, n, mu, m, neighbours, group_indices, device):
+    def __init__(
+        self, N, n, mu, m, neighbour_id, neighbour_distance, clustering_labels
+    ):
         super(NRBS, self).__init__()
         self.N = N
         self.n = n
         self.mu = mu
         self.m = m
-        self.neighbours = neighbours.to(device)
-        self.group_indices = group_indices
+        self.neighbour_id = neighbour_id
+        self.neighbour_distance = neighbour_distance
+        self.clustering_labels = clustering_labels
 
         self.encoder = torch.nn.Linear(self.N, self.n)
-        self.decoder = torch.nn.Linear(self.n, self.N)
+        # self.decoder = torch.nn.Linear(self.n, self.N)
+        self.decoder = torch.nn.Parameter(torch.Tensor(self.n, self.N))
         self.bandwidth_layers = torch.nn.Linear(self.n, self.n * self.m)
 
     def encode(self, x):
         return self.encoder(x)
 
-    def get_group_idx_smoothed_basis(self, groud_idx, basises, bubbles):
-        curr_bubble = bubbles[:, :, groud_idx, :]
-        curr_basis = basises[:, self.group_indices[groud_idx], :]
+    # distance: N x mu
+    # w: n x N ([0 element_size])
+    # mu: number of neighbour elements
+    def bubble(self, neighbour_distance, w, mu):
+        print(w.device)
+        print(neighbour_distance.device)
+        n = w.shape[0]
+        window = torch.relu(
+            -(neighbour_distance.unsqueeze(0).expand(n, -1, -1) ** 2)
+            / (w.unsqueeze(-1) * mu) ** 2
+            + 1
+        )
+        window = window / torch.sum(window, dim=1, keepdim=True)
+        # n x N x mu
+        return window
 
-        # n x mu x N/m
-        curr_basis = curr_basis.permute((0, 2, 1))
-
-        # n x batch x mu
-        curr_bubble = curr_bubble.permute((1, 0, 2))
-
-        # n x b x N/m
-        return torch.bmm(curr_bubble, curr_basis)
+    # x: n x N
+    # neighbour_id: N x mu
+    # w: n x N
+    def convolve(self, x, neighbour_id, neighbour_distance, w, mu):
+        # n x N x mu
+        bubbles = self.bubble(neighbour_distance, w, mu)
+        return torch.sum(x[:, neighbour_id] * bubbles, dim=-1)
 
     def decode(self, encoded):
-        batch_size = encoded.shape[0]
-        vmap_bubble = vmap(self.bubble, in_dims=0)
-        vmap_vmap_bubble = vmap(vmap_bubble, in_dims=0)
-        vmap_vmap_vmap_bubble = vmap(vmap_vmap_bubble, in_dims=0)
+        # n x m
+        bandwidths = self.bandwidth_layers(encoded)
+        bandwidths = bandwidths.reshape(self.n, self.m)
 
-        # batch size x n x m
-        bandwidths = torch.bmm(
-            encoded.repeat(self.n, 1, 1), self.bandwidth_layers
-        ).permute(1, 0, 2)
-        bandwidths = torch.sigmoid(bandwidths)
-        # batch size x n x m x mu
-        bubbles = vmap_vmap_vmap_bubble(bandwidths)
+        # n x N
+        bandwidths = bandwidths[:, self.clustering_labels]
 
-        node_idxs = torch.linspace(0, self.N - 1, self.N, dtype=torch.long)
-        basises = self.decoder[:, self.getNeighbours(node_idxs)]
-
-        # n x batch_size x N
-        smoothed_basis = torch.zeros((self.n, batch_size, self.N), device=self.device)
-        for i in range(len(self.group_indices)):
-            # n x b x N/m
-            smoothed_basis[
-                :, :, self.group_indices[i]
-            ] = self.get_group_idx_smoothed_basis(i, basises, bubbles)
-
-        # batch x n x N
-        smoothed_basis = smoothed_basis.permute((1, 0, 2))
-
-        # # batch size x n x N
-        # smoothed_basis = self.smooth_basis(bubbles=bubbles)
-
-        # batch size x 1 x n
-        encoded = encoded.unsqueeze(2).permute((0, 2, 1))
+        # n x N
+        convolved_basis = self.convolve(
+            self.decoder,
+            self.neighbour_id,
+            self.neighbour_distance,
+            bandwidths,
+            self.mu,
+        )
         # batch size x N
-        return torch.bmm(encoded, smoothed_basis).squeeze(1)
+        return torch.matmul(encoded, convolved_basis)
 
     def forward(self, x):
         return self.decode(self.encode(x))
 
-    def bubble(self, w):
-        x = torch.arange(self.mu, device=self.device)
-        window = torch.relu(-(x**2) / (w * self.mu) ** 2 + 1)
-        window = window / torch.sum(window)
-        return window
-
-    def getNeighbours(self, idx):
-        return self.neighbours[idx]
-
-    def convolve(self, x, bubble):
-        return x * bubble
-
-    def smooth_basis(self, bubbles):
-        node_idxs = torch.linspace(0, self.N - 1, self.N, dtype=torch.long)
-        return torch.stack(
-            [
-                self.smooth_vec(i, node_idx=node_idxs, bubbles=bubbles)
-                for i in range(self.n)
-            ],
-            dim=1,
-        ).squeeze(2)
-
-    def smooth_vec(self, basis_idx, node_idx, bubbles):
-        return (self.decoder[basis_idx][self.getNeighbours(node_idx)] * bubbles).sum(
-            dim=2
-        )
-
-    def smooth(self, basis_idx, node_idx, bubbles):
-        return torch.sum(
-            self.decoder[basis_idx][self.getNeighbours(node_idx)]
-            * bubbles[basis_idx][node_idx]
-        )
-
 
 class EncoderDecoder(torch.nn.Module):
-    def __init__(self, N, n, mu, m, neighbours, group_indices, device):
+    def __init__(
+        self, N, n, mu, m, neighbour_id, neighbour_distance, clustering_labels, device
+    ):
         super(EncoderDecoder, self).__init__()
         self.nrbs = NRBS(
             N=N,
             n=n,
             mu=mu,
             m=m,
-            neighbours=neighbours,
-            group_indices=group_indices,
-            device=device,
+            neighbour_id=neighbour_id.to(device),
+            neighbour_distance=neighbour_distance.to(device),
+            clustering_labels=clustering_labels.to(device),
         ).to(device)
         self.device = device
 
